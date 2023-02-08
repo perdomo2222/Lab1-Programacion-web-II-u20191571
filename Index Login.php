4894ef09bbc72a506d6de952c5722f9df8af0455
perdomo2222 <kiuby2608@gmail.com>
2023-02-08 17:04:57 -0600

    camios actualizados

--- Index Login.php
+++ Index Login.php
@@ -6,8 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Login</title>
+    <title>Login sv</title>
 </head>
+
 
 <body class="bg-info d-flex justify-content-center align-items-center vh-100">
     <div class="bg-white p-5  rounded-3 text-secondary" style="width: 25rem">
